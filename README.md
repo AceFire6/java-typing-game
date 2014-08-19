a77889ca1ecd6a71e30b665b1487a64b9a522f75
Jethro Muller <JethroMuller@gmail.com>
2014-08-19 10:18:06 +0200

    Initial Commit
    Everything in src at this point has been given as part of the assignment.
    example_dict.txt was also given.
    Add start of the README.md.

---
+++ README.md
@@ -0,0 +1,13 @@
+# Assignment 2 - Safety with Concurrency and Synchronization
+#### Author: Jethro Muller
+#### Student Number: MLLJET001
+#### Date: 19 August 2014
+
+## Description
+
+
+## Instructions
+
+
+## File List
+
---
+++ src/WordPanel.java
@@ -0,0 +1,44 @@
+
+
+import javax.swing.*;
+import java.awt.*;
+
+public class WordPanel extends JPanel implements Runnable {
+    public static volatile boolean done;
+    private WordRecord[] words;
+    private int noWords;
+    private int maxY;
+
+
+    public void paintComponent(Graphics g) {
+        int width = getWidth();
+        int height = getHeight();
+        g.clearRect(0,0,width,height);
+        g.setColor(Color.red);
+        g.fillRect(0,maxY-10,width,height);
+
+        g.setColor(Color.black);
+        g.setFont(new Font("Helvetica", Font.PLAIN, 26));
+        //draw the words
+        //animation must be added
+        for (int i=0;i<noWords;i++){
+            //g.drawString(words[i].getWord(),words[i].getX(),words[i].getY());
+            g.drawString(words[i].getWord(),words[i].getX(),words[i].getY()+20);  //y-offset for skeleton so that you can see the words
+        }
+
+    }
+
+    WordPanel(WordRecord[] words, int maxY) {
+        this.words=words; //will this work?
+        noWords = words.length;
+        done=false;
+        this.maxY=maxY;
+    }
+
+    public void run() {
+        //add in code to animate this
+    }
+
+}
+
+
